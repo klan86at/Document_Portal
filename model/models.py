4808ddc737573aab9becb8a792c2fa26ebd565bf
klan86at <klang86at@gmail.com>
2025-08-15 02:29:28 +0300

    Document analysis updated

--- model/models.py
+++ model/models.py
@@ -1,2 +1,20 @@
+from ctypes import Union
+from annotated_types import T
+from langchain_text_splitters import Language
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict, Any, Union
+
+
+class Metadata(BaseModel):
+    Summary: List[str] = Field(default_factory=list, description="A list of summary points extracted from the document.")]
+    """
+    Represents the metadata associated with a document.
+    """
+    Title: Optional[str] = Field(None, description="The title of the document.")
+    Author: Optional[str] = Field(None, description="The author of the document.")
+    DateCreated: Optional[str] = Field(None, description="The creation date of the document.")
+    LastModifiedDate: Optional[str] = Field(None, description="The last modification date of the document.")
+    Publisher: Optional[List[str]] = Field(None, description="The publisher of the document.")
+    Language: Optional[str] = Field(None, description="The language of the document.")
+    PageCount: Union[int, str] = Field(..., description="The number of pages in the document.")
+    SentimentTone: str = Field(..., description="The sentiment tone of the document.")
--- src/doc_analyzer/data_analysis.py
+++ src/doc_analyzer/data_analysis.py
@@ -1,10 +1,14 @@
+from itertools import chain
 import os
+import sys
+from urllib import response
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from promptlib.prompt_library import *
 
 class DocumentAnalyzer:
     """
@@ -12,21 +16,43 @@ class DocumentAnalyzer:
     Automatically logs all actions and supports session-based organization.
     """
     def __init__(self):
-        pass
-        # self.llm = ModelLoader().load_llm()
-        # self.parser = JsonOutputParser()
-        # self.fixing_parser = OutputFixingParser.from_llm(llm=self.llm, parser=self.parser)
-        # self.log = CustomLogger().get_logger(__name__)
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(llm=self.llm, parser=self.parser)
+        
+            self.prompt = prompt
 
-    def analyze_document(self):
+            self.log.info("DocumentAnalyzer initialized successfully.")
+        
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error initializing DocumentAnalyzer", sys)
+        
+
+    def analyze_document(self, document_text:str)-> dict:
         """
         Analyze the given document text using the LLM model.
         """
-        pass
-        # try:
-        #     model = self.model_loader.load_model()
-        #     # Assuming the model has a method 'analyze' to process the document
-        #     analysis_result = model.analyze(document_path)
-        # except Exception as e:
-        #     raise DocumentPortalException("Error analyzing document", e) from e
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+
+            self.log.info("Metadata analysis chain initialized")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "text": document_text
+            })
+
+            self.log.info("Metadata analysis completed", keys=list(response.keys()))
+
+            return response
+        
+        except Exception as e:
+            self.log.error("Error analyzing metadata", error=str(e))
+            raise DocumentPortalException("Metadata extraction failed", e) from e
+        
     
