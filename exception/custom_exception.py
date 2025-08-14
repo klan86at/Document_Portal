158b03f5d55be2086316146838edc4ed3cc6086f
klan86at <klang86at@gmail.com>
2025-08-15 01:30:37 +0300

    Data ingestion updated

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,11 +1,15 @@
+from os import error
 import sys
 import traceback
+from typing import Any
 from logger.custom_logger import CustomLogger
 logger=CustomLogger().get_logger(__file__)
 
+
 class DocumentPortalException(Exception):
     """Custom exception for Document Portal"""
-    def __init__(self, error_message:str, error_details:sys):
+    def __init__(self, error_message:str, error_details: Any=None):
+        super().__init__(error_message)
         _,_,exc_tb=error_details.exc_info()
         self.file_name=exc_tb.tb_frame.f_code.co_filename
         self.lineno=exc_tb.tb_lineno
@@ -24,6 +28,6 @@ if __name__ == "__main__":
         a = 1 / 0
         print(a)
     except Exception as e:
-        app_exc=DocumentPortalException(e, sys)
+        app_exc=DocumentPortalException(str(e), sys)
         logger.error(app_exc)
         raise app_exc
--- src/doc_analyzer/data_ingestion.py
+++ src/doc_analyzer/data_ingestion.py
@@ -1,9 +1,13 @@
+from fileinput import filename
 import os
 import fitz
 import uuid
 from datetime import datetime
+
+from requests import session
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
+from src import doc_analyzer
 
 class DocumentHandler:
     """
@@ -11,9 +15,80 @@ class DocumentHandler:
     Automatically logs all actions and supports session-based organization.
     """
 
-    def __int__(self):
-        pass
-    def save_pdf(self):
-        pass
-    def read_pdf(self):
-        pass
+    def __init__(self, data_dir=None, session_id=None):
+        try:
+            self.log=CustomLogger().get_logger(__name__)
+            self.data_dir = data_dir or os.getenv(
+                "DATA_STORAGE_PATH",
+                os.path.join(os.getcwd(), "data", "doc_analysis")
+            )
+            self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+
+            # Create base session directory
+            self.session_path = os.path.join(self.data_dir, self.session_id)
+            os.makedirs(self.session_path, exist_ok=True)
+
+            self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
+        except Exception as e:
+            self.log.error("Error initializing DocumentHandler", {e})
+            raise DocumentPortalException("Error initializing PDFHandler", e) from e
+        
+    def save_pdf(self, uploaded_file):
+        try:
+            filename = os.path.basename(uploaded_file.name)
+            if not filename.lower().endswith('.pdf'):
+                raise DocumentPortalException("Uploaded file is not a PDF")
+            
+            save_path = os.path.join(self.session_path, filename)
+
+            with open(save_path, 'wb') as f:
+                f.write(uploaded_file.getbuffer())
+
+            self.log.info("PDF saved successfully", file=filename, save_path=save_path, session_id=self.session_id)
+            return save_path
+        except Exception as e:
+            self.log.error(f"Error saving PDF: {e}")
+            raise DocumentPortalException("Error saving PDF", e) from e
+
+    def read_pdf(self, pdf_path:str) -> str:
+        try:
+            text_chunks = []
+            with fitz.open(pdf_path) as doc:
+                for page_num in range(len(doc)):
+                    page = doc[page_num]
+                    text_chunks.append(f"\n--- Page {page_num + 1} ---\n{page.get_text()}")
+            text = "\n".join(text_chunks)
+
+            self.log.info("PDF read successfully", pdf_path=pdf_path, session_id=self.session_id, pages=len(text_chunks))
+            return text
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("Error reading PDF", e) from e
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from io import BytesIO
+    handler = DocumentHandler()
+
+    pdf_path =r"D:\\LLMOPs\\Document_Portal\\data\\doc_analysis\\AppliedAI_White_Paper_Retrieval-augmented-Generation-Realized_FINAL_20240618.pdf"
+
+    class DummyFile:
+        def __init__(self, file_path):
+            self.name = Path(file_path).name
+            self.file_path = file_path
+
+        def getbuffer(self):
+            return open(self.file_path, "rb").read()
+        
+    dummy_pdf = DummyFile(pdf_path)
+
+    handler = DocumentHandler(session_id="test_session")
+
+    try:
+        saved_path = handler.save_pdf(dummy_pdf)
+        print(saved_path)
+
+        content = handler.read_pdf(saved_path)
+        print(content[:100])
+    except Exception as e:
+        print(f"Error: {e}")
