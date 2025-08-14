3d37b380c924255c77b5ecdd9fef5e7cae4de4b5
klan86at <klang86at@gmail.com>
2025-08-14 09:48:36 +0300

    Document analyzer created

---
+++ src/doc_analyzer/data_analysis.py
@@ -0,0 +1,33 @@
+import os
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentAnalyzer:
+    """
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
+    """
+    def __init__(self):
+        pass
+        # self.llm = ModelLoader().load_llm()
+        # self.parser = JsonOutputParser()
+        # self.fixing_parser = OutputFixingParser.from_llm(llm=self.llm, parser=self.parser)
+        # self.log = CustomLogger().get_logger(__name__)
+
+    def analyze_metadata(self):
+        """
+        Analyze the given document text using the LLM model.
+        """
+        pass
+        # try:
+        #     model = self.model_loader.load_model()
+        #     # Assuming the model has a method 'analyze' to process the document
+        #     analysis_result = model.analyze(document_path)
+        # except Exception as e:
+        #     raise DocumentPortalException("Error analyzing document", e) from e
+    
+    
