0117bf8edf5bfbd1bdfe83e8dcb657c6af6c8238
klan86at <klang86at@gmail.com>
2025-08-18 20:50:35 +0300

    skeleton multi doc module updated

---
+++ src/multi_doc_chat/data_ingestion.py
@@ -0,0 +1,9 @@
+class DocumentIngestor:  # cSpell:ignore Ingestor
+    def __init__(self):
+        pass
+
+    def ingest_file(self):
+        pass
+
+    def _create_retriever(self):
+        pass
---
+++ src/multi_doc_chat/retreival.py
@@ -0,0 +1,19 @@
+class ConversationalRAG:
+    def __init__(self):
+        pass
+
+    def load_retriever_from_faiss(self):  # cSpell:ignore faiss
+        pass
+
+    def invoke(self):
+        pass
+
+    def _load_llm(self):
+        pass
+
+    @staticmethod
+    def _format_docs(docs):
+        pass
+
+    def _build_lcel_chain(self):  # cSpell:ignore lcel
+        pass
